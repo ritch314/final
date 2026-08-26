dd926f74bd6641345b6cf5c35d43e461853956a5
ritch314 <ritchelynroquero6@gmail.com>
2026-08-26 20:30:49 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ const siteConfig = {
   signature: "Choy",
   relationshipStart: "the night we started talking",
 
-  musicFile: "assets/audio/song.emp3.mp3",
+  musicFile: "assets/audio/song.mp3.mp3",
 
   rememberAnswerLocally: true,
 
