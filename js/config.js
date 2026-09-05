3119956311e6f0588cfd95d632dec507bb9ca869
ritch314 <ritchelynroquero6@gmail.com>
2026-09-05 14:23:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-
-
 const siteConfig = {
   herName: "Langga",
   herNickname: "babyyy",              // optional — shown instead of herName in the greeting if set
@@ -8,29 +6,23 @@ const siteConfig = {
   herLocation: "Your Location",
   signature: "Choy",
   relationshipStart: "the night we started talking",
-
   musicFile: "assets/audio/song.mp3.mp3",
-
   rememberAnswerLocally: true,
-
   notifyOnAnswer: true,
   formspreeFormId: "xljrvyra"   
 };
-
 const traits = [
   { icon: "01 · smile",       title: "Your Smile",        text: "There's something about the way you smile that can instantly make a conversation better. Pano nga yung smile langga:)?" },
   { icon: "02 · personality", title: "Your Personality",  text: "I like the way you are yourself. You don't have to try too hard to be interesting. Kaya sayo lang ako interested hard pass sa iba" },
   { icon: "03 · talks",       title: "Your Conversations",text: "Somehow, talking to you makes time disappear. Ang corny ko talaga kahit kailan" },
   { icon: "04 · details",     title: "Your Little Things",text: "Sometimes it's not the big things. It's the small details that make someone special." }
 ];
-
 const timeline = [
   { day: "Day 01", text: "We started talking sa tiktok as if matagal na tayo magkakilala. Nangupal agad ako sayo HAHAHAH" },
   { day: "Day 05", text: "Our conversations started getting longer. Alam kung na may nararamdaman na ako para sayo pero deadma lang ako at pinagmumura pa kita. I'm sorry babyyy" },
   { day: "Day 10", text: "I caught myself smiling at my phone. Para akong ewan hahahah wala ang saya ko pag kausap kita buang talaga" },
   { day: "Day 15", text: "I realized... Na gusto kita. Kaya nag confess ako sayo kahit d ako sure kung e reciprocate mo yung feelings ko. Nag take a risk si choy" }
 ];
-
 const promises = [
   { title: "I'll make an effort.", text: "I won’t let distance become an excuse to stop trying. If you’re worth finding my way to, then I’ll keep finding my way to you." },
   { title: "I'll respect you.",    text: "Your comfort and feelings will always matter to me. I’ll never want you to feel pressured or rushed. I want you to feel safe being yourself with me." },
@@ -38,7 +30,6 @@ const promises = [
   { title: "I'll communicate.",    text: "I’d rather have an honest conversation than let a misunderstanding create distance between us." },
   { title: "I'll keep being myself.", text: "No pretending kasi d ko ugali yan. No games kasi totoo yung nararamdaman ko sayo. Just me being me." }
 ];
-
 const gallery = [
   { img: "assets/images/1.jpg", caption: "That one conversation that went on for hours hanggang madaling araw" },
   { img: "assets/images/2.jpg", caption: "The first time you sent that laugh, I really love the way you laugh and alam mo yan" },
